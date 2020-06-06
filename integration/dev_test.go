57114aa8ea5843af16d012a08f56a39f2830e2a8
gsquared94 <gaghosh@google.com>
2020-06-06 15:48:46 -0700

    Fix broken test

--- integration/dev_test.go
+++ integration/dev_test.go
@@ -26,6 +26,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/GoogleContainerTools/skaffold/pkg/skaffold/constants"
 	"github.com/sirupsen/logrus"
 	appsv1 "k8s.io/api/apps/v1"
 	"k8s.io/apimachinery/pkg/util/wait"
@@ -354,6 +355,10 @@ func createModifiedKubeconfig(namespace string) ([]byte, string, error) {
 	contextName := "modified-context"
 	if config.IsKindCluster(kubeConfig.CurrentContext) {
 		contextName = "kind-" + contextName
+	}
+
+	if kubeConfig.CurrentContext == constants.DefaultMinikubeContext {
+		contextName = kubeConfig.CurrentContext // multiple contexts not available for a single minikube cluster
 	}
 
 	activeContext := kubeConfig.Contexts[kubeConfig.CurrentContext]
